979b0e3c136db33716f4ff715a3001091cf41cd0
msaaad <saad99u@gmail.com>
2021-01-12 02:19:19 +0500

    learning prediction

--- deepLearning.py
+++ deepLearning.py
@@ -1,6 +1,8 @@
+# neural networks greate at feeding, just dont overfeed
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 mnist = tf.keras.datasets.mnist
 
@@ -22,6 +24,26 @@ model.compile(optimizer='adam',
 
 model.fit(x_train, y_train, epochs=3)
 
-plt.imshow(x_train[0], cmap=plt.cm.binary)
+# loss and accuracy calculation
+val_loss, val_acc = model.evaluate(x_test, y_test)
+print(val_loss, val_acc)
+
+# saving model
+model.save('number_reader.model')
+
+new_model = tf.keras.models.load_model('number_reader.model')
+
+predictions = new_model.predict([x_test])
+
+print(predictions)
+
+print(np.argmax(predictions[0]))
+
+# plot showing
+# plt.imshow(x_train[0], cmap=plt.cm.binary)
+# plt.show()
+# print(x_train[0])
+
+# for checking the prediction
+plt.imshow(x_test[0])
 plt.show()
-print(x_train[0])
